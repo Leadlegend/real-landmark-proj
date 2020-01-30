c5960db51e7094873443165d852f94a38bcd2ddc
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-01-30 23:03:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from __future__ import print_function, absolute_import, division
 
 from utils.init import load_data,get_data
 from utils.models import IMMModel
+import tensorflow as tf
+import tensorlayer as tl
 
-batch_size=50
-shape=(batch_size,96,96,1)
+batch_size=3
+shape=(None,96,96,None)
 n_features=10
 
 def main():
-	dataset=load_data()
+	dataset=load_data(batch_size=batch_size)
 	model=IMMModel(shape,n_features)
 	img1,img2=get_data(dataset)
-	img3=model.train(img1)
-	print(img3)
+
+	img=model.train((img1,img2))
+	print(img)
 
 if __name__=="__main__":
 	main()
